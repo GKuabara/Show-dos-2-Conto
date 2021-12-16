02320c0810af3bf912933761afede8cc65068497
bmarquescost <b.marquescost@gmail.com>
2021-12-16 17:25:49 -0300

    Commenting datset code

--- source/dataset.cpp
+++ source/dataset.cpp
@@ -1,3 +1,7 @@
+// Gabriel Alves Kuabara - nUSP 11275043
+// Bernardo Marques Costa - nUSP 11795551
+// Victor Henrique da Silva - nUSP 11795759
+
 #include <fstream>
 #include <string>
 #include <random>
@@ -8,24 +12,38 @@
 
 using json = nlohmann::json;
 
+/**
+ * Função convertLevelToInt:
+ * - Parâmetro: string que representa o nível de dificuldade
+ * - Retorno: nível de dificuldade como inteiro
+ */
 static int convertLevelToInt(string l) {
     if (l.compare("Easy") == 0) return 0;
     else if (l.compare("Medium") == 0) return 1;
     return 2;
 }
 
+// Função Construtora vazia
 Dataset::Dataset() {}
 
-// Load dataset of Question from json file 
+
+/**
+ * Construtora da classe Dataset:
+ * - Parâmetro: recebe uma string que representa o path até o arquivo json
+ * Carrega um objeto json, para que seja possível deserializar as perguntas e construir o dataset de perguntas
+ */
 Dataset::Dataset(string filename) {
+    // Leitura do json
     std::ifstream i(filename);
     json j;
     i >> j;
     
+    // Criando cada chave do map associada a um vetor vazio
     this->questions[convertLevelToInt("Easy")] = vector<Question> ();
     this->questions[convertLevelToInt("Medium")] = vector<Question> (); 
     this->questions[convertLevelToInt("Hard")] = vector<Question> (); 
     
+    // Realizando o carregamento do map
     for (auto it = j.begin(); it != j.end(); ++it) {
         vector<string> options {j[it.key()]["options"][0], j[it.key()]["options"][1], j[it.key()]["options"][2], j[it.key()]["options"][3]};
         Question q = Question(j[it.key()]["question"], j[it.key()]["answer"], options, convertLevelToInt(j[it.key()]["level"]));
@@ -40,6 +58,11 @@ auto selectRandom(const S &s, size_t n) {
     return it;
 }
 
+/**
+ * Função getRandomQuestion:
+ * - Parâmetro: recebe uma string que representa o nível de dificuldade desejado da pergunta
+ * - Retorno: retorna aleatóriamente dentro do map um objeto  do tipo Question nivelado de acordo com o parâmetro
+ */
 Question Dataset::getRandomQuestion(string level_str) {
     int level = convertLevelToInt(level_str);
     std::srand(std::time(nullptr));
